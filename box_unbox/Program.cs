99bd61edbd697241a723cda6444af3628ed70b44
David Williams <willidv@gmail.com>
2017-10-31 08:37:17 -0500

    csharp

---
+++ box_unbox/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace box_unbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Sum = 0;
+            List<int> intlist = new List<int>();
+            List<object> blist = new List<object>();
+            blist.Add(7);
+            blist.Add(-1);
+            blist.Add(true);
+            blist.Add("chair");
+        
+            foreach(object i in blist){
+                Console.WriteLine(i);
+                if (i is int){
+                    Sum += Convert.ToInt32(i);
+                    Console.WriteLine(Sum);
+                }
+            }   
+        }
+    }
+}
